4efbee8f4d28071d62d109cecb890c2d04de94e9
FailC <f.nikodem@web.de>
2024-04-30 09:43:48 +0200

    first commit

---
+++ src/main.rs
@@ -0,0 +1,136 @@
+use std::env;
+use std::fs;
+use std::fs::File;
+use std::fs::OpenOptions;
+use std::io;
+use std::io::prelude::*;
+use std::path::PathBuf;
+use std::process::ExitCode;
+//use dirs;
+// notes -arg text
+// notes -n neue notiz
+// notes -d notiz löschen
+// notes -l list
+// notes    list
+// load text file
+// if it doesnt exist, create it,
+// ~/home/user/.notes
+
+fn check_and_create_file() -> Result<PathBuf, io::Error> {
+    if let Some(home_dir) = dirs::home_dir() {
+        let file_path = home_dir.join(".notes_storage");
+        if fs::metadata(&file_path).is_ok() {
+            //println!("file exist");
+        } else {
+            let _ = File::create(&file_path)?;
+            println!("new note file created");
+        }
+        Ok(file_path)
+    } else {
+        // If home directory cannot be retrieved, return an error
+        Err(io::Error::new(
+            io::ErrorKind::NotFound,
+            "Home directory not found",
+        ))
+    }
+}
+
+fn new_note(args: &[String], mut file: &File) -> std::io::Result<()> {
+    // parse line into vec?
+    let mut note: Vec<String> = Vec::new();
+    for arg in args.iter().skip(2) {
+        if !arg.trim().is_empty() {
+            note.push(arg.to_owned());
+        }
+    }
+    if note.is_empty() {
+        return Ok(());
+    }
+    let content = note.join(" ");
+    println!("{}", &content);
+    file.write_all(content.as_bytes())?;
+    file.write_all("\n".as_bytes())?;
+    println!("note created");
+
+    Ok(())
+}
+
+fn list_notes(mut file: &File) -> std::io::Result<()> {
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    let lines: Vec<String> = contents.lines().map(String::from).collect();
+    for line in lines {
+        println!("{line}");
+    }
+    Ok(())
+}
+
+fn delete_note(mut file: &File) -> std::io::Result<()> {
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    let lines: Vec<String> = contents.lines().map(String::from).collect();
+    for (i, line) in lines.iter().enumerate() {
+        println!("{i}: {line}", i = i + 1);
+    }
+    print!("Select note to delete: ");
+    //let selection = 
+
+    let mut input = String::new();
+
+    io::stdin().read_line(&mut input)?;
+    // Parse the input into an integer
+    let num: i8 = input.trim().parse()
+        .expect("Please enter a valid number");   
+
+
+
+
+    // delete stuff in vector and write all to file
+
+    Ok(())
+}
+
+
+fn main() -> ExitCode {
+    let file_path = match check_and_create_file() {
+        Ok(fp) => fp,
+        Err(_) => panic!("ERROR: file fucked up"),
+    };
+
+    let file = match OpenOptions::new().read(true).append(true).open(file_path) {
+        Ok(file) => file,
+        Err(_) => panic!("ERROR: can't open file"),
+    };
+
+    //let mut args: Vec<String> = env::args().collect();
+    let args: Vec<String> = env::args().collect();
+    if args.len() <= 1 {
+        eprintln!("ERROR: no argument provided");
+        return ExitCode::FAILURE;
+    }
+    // arg 1 => n, d, l
+    // arg 2.. => new note, if arg1
+
+    if args[1] == "n" {
+        match new_note(&args, &file) {
+            Ok(()) => (),
+            Err(err) => panic!("ERROR: {err}"),
+        }
+    } 
+    else if args[1] == "l" {
+        match list_notes(&file) {
+            Ok(()) => (),
+            Err(err) => panic!("ERROR: {err}"),
+        }
+    }
+    else if args[1] == "d" {
+            match delete_note(&file) {
+            Ok(()) => (),
+            Err(err) => panic!("ERROR: {err}"),
+        }
+    }
+    else {
+        eprintln!("wrong argument");
+    }
+    ExitCode::SUCCESS
+}
